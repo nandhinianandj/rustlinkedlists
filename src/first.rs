522a3c7452a4a46be4827315909954fbb1eca8f8
Yaay Nands <16502598+nandhinianandj@users.noreply.github.com>
2024-12-05 21:49:17 +0530

    (/usr/share/games/fortunes/osfortune) % "For something that does not exist, the Internet Engineering Task Force has has quite an impact."
      -- Scott Bradner (Open Sources, 1999 O'Reilly and Associates)

--- src/first.rs
+++ src/first.rs
@@ -1,8 +1,12 @@
 // in first.rs
+struct Node {
+    elem: i32,
+    next: List,
+}
 
 // pub says we want people outside this module to be able to use List
 pub enum List {
     Empty,
-    Elem(i32, Box<List>),
+    More(Box<Node>),
 }
 
