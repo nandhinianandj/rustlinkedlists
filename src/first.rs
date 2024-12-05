d40ab5b69a12294169f78c7641cc3b8ef7e2abe7
Yaay Nands <16502598+nandhinianandj@users.noreply.github.com>
2024-12-05 22:29:13 +0530

    (/usr/share/games/fortunes/pets) % It's always sad when the fleas leave, because that means your dog is dead. 		-- Wesley T. Williams

--- src/first.rs
+++ src/first.rs
@@ -1,12 +1,44 @@
-// in first.rs
-struct Node {
-    elem: i32,
-    next: List,
+use std::mem;
+
+pub struct List {
+    head: Link,
 }
 
-// pub says we want people outside this module to be able to use List
-pub enum List {
+enum Link {
     Empty,
     More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    // TODO, make code happen
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+        elem: elem,
+        next: mem::replace(&mut self.head, Link::Empty),
+        });
+        self.head = Link::More(new_node);
+    }
+    pub fn pop(&mut self) -> Option<i32> {
+        let result;
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => {
+                result = None;
+                }
+            Link::More(node) => {
+                result = Some(node.elem);
+                self.head = node.next;
+            }
+    };
+    result
+}
+
 }
 
