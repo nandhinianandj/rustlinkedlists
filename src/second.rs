672a011a6b45a0a0bfd1954208c747b8af7f8dc0
Yaay Nands <16502598+nandhinianandj@users.noreply.github.com>
2024-12-06 23:14:52 +0530

    (/usr/share/games/fortunes/linux) % /*  * [...] Note that 120 sec is defined in the protocol as the maximum  * possible RTT.  I guess we'll have to use something other than TCP  * to talk to the University of Mars.  * PAWS allows us longer timeouts and large windows, so once implemented  * ftp to mars will work nicely.  */ 	-- from /usr/src/linux/net/inet/tcp.c, concerning RTT [round trip time]

--- src/second.rs
+++ src/second.rs
@@ -1,13 +1,8 @@
-use std::mem;
-
 pub struct List {
     head: Link,
 }
 
-enum Link {
-    Empty,
-    More(Box<Node>),
-}
+type Link = Option<Box<Node>>;
 
 struct Node {
     elem: i32,
@@ -16,81 +11,35 @@ struct Node {
 
 impl List {
     pub fn new() -> Self {
-        List { head: Link::Empty }
-    }
-    pub fn push(&mut self, elem: i32) {
-        let new_node = Box::new(Node {
-        elem: elem,
-        next: mem::replace(&mut self.head, Link::Empty),
-        });
-        self.head = Link::More(new_node);
-    }
-    pub fn pop(&mut self) -> Option<i32> {
-        let result;
-        match mem::replace(&mut self.head, Link::Empty) {
-            Link::Empty => {
-                result = None;
-                }
-            Link::More(node) => {
-                result = Some(node.elem);
-                self.head = node.next;
-            }
-        };
-        result
+        List { head: None }
     }
 
-}
-impl Node {
-    fn pop_node(&mut self) -> Link {
-        let cur_link = mem::replace(&mut self.next, Link::Empty);
-        cur_link
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: self.head.take(),
+        });
+
+        self.head = Some(new_node);
     }
-}
-impl Drop for List {
-    fn drop(&mut self) {
-        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
-        // `while let` == "do this thing until this pattern doesn't match"
-        while let Link::More(mut boxed_node) = cur_link {
-            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
-            // boxed_node goes out of scope and gets dropped here;
-            // but its Node's `next` field has been set to Link::Empty
-            // so no unbounded recursion occurs.
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match self.head.take() {
+            None => None,
+            Some(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
         }
     }
 }
 
-
-#[cfg(test)]
-mod test {
-    use super::List;
-    #[test]
-    fn basics() {
-        let mut list = List::new();
-
-        // Check empty list behaves right
-        assert_eq!(list.pop(), None);
-
-        // Populate list
-        list.push(1);
-        list.push(2);
-        list.push(3);
-
-        // Check normal removal
-        assert_eq!(list.pop(), Some(3));
-        assert_eq!(list.pop(), Some(2));
-
-        // Push some more just to make sure nothing's corrupted
-        list.push(4);
-        list.push(5);
-
-        // Check normal removal
-        assert_eq!(list.pop(), Some(5));
-        assert_eq!(list.pop(), Some(4));
-
-        // Check exhaustion
-        assert_eq!(list.pop(), Some(1));
-        assert_eq!(list.pop(), None);
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = self.head.take();
+        while let Some(mut boxed_node) = cur_link {
+            cur_link = boxed_node.next.take();
+        }
     }
 }
-
 
