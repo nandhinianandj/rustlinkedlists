0cf19768e212c22518304616e5f19b4d3e1ad4bd
Yaay Nands <16502598+nandhinianandj@users.noreply.github.com>
2025-01-14 09:52:44 +0000

    (/usr/share/games/fortunes/pets) % I love dogs, but I hate Chihuahuas.  A Chihuahua isn't a dog.  It's a rat with a thyroid problem.

--- src/second.rs
+++ src/second.rs
@@ -63,61 +63,64 @@ impl<T> Iterator for IntoIter<T> {
     }
 }
 
-#[test]
-fn basics() {
-    let mut list = List::new();
+#[cfg(test)]
+mod test {
+    use super::List;
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+    
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+    #[test]
+    fn peek_mut() {
+        let mut list = List::new();
+        assert_eq!(list.peek_mut(), None);
+        list.push(1); list.push(2); list.push(3);
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+        list.peek_mut().map(|value| {
+            *value = 42
+        });
 
-    // Check empty list behaves right
-    assert_eq!(list.pop(), None);
-
-    // Populate list
-    list.push(1);
-    list.push(2);
-    list.push(3);
-
-    // Check normal removal
-    assert_eq!(list.pop(), Some(3));
-    assert_eq!(list.pop(), Some(2));
-
-    // Push some more just to make sure nothing's corrupted
-    list.push(4);
-    list.push(5);
-
-    // Check normal removal
-    assert_eq!(list.pop(), Some(5));
-    assert_eq!(list.pop(), Some(4));
-
-    // Check exhaustion
-    assert_eq!(list.pop(), Some(1));
-    assert_eq!(list.pop(), None);
+    }
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        list.push(1); list.push(2); list.push(3);
+    
+        assert_eq!(list.peek(), Some(&3));
+    
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.pop(), Some(3));
+    }
+    
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+    
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+    }
 }
-
-#[test]
-fn peek() {
-    let mut list = List::new();
-    assert_eq!(list.peek(), None);
-    assert_eq!(list.peek_mut(), None);
-    list.push(1); list.push(2); list.push(3);
-
-    assert_eq!(list.peek(), Some(&3));
-    assert_eq!(list.peek_mut(), Some(&mut 3));
-    list.peek_mut().map(|value| {
-        *value = 42
-    });
-
-    assert_eq!(list.peek(), Some(&42));
-    assert_eq!(list.pop(), Some(42));
-}
-
-#[test]
-fn into_iter() {
-    let mut list = List::new();
-    list.push(1); list.push(2); list.push(3);
-
-    let mut iter = list.into_iter();
-    assert_eq!(iter.next(), Some(3));
-    assert_eq!(iter.next(), Some(2));
-    assert_eq!(iter.next(), Some(1));
-    assert_eq!(iter.next(), None);
-}
-
